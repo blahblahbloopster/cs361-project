89c809f8f3013dadbf88ebffd78d0c2cacfbec16
Max <35409580+blahblahbloopster@users.noreply.github.com>
2025-07-25 16:46:16 -0700

    add deployment channel status UI

--- app/src/main/java/com/github/osuasdt/groundstation/MainActivity.kt
+++ app/src/main/java/com/github/osuasdt/groundstation/MainActivity.kt
@@ -4,13 +4,38 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material3.Badge
+import androidx.compose.material3.ColorScheme
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Shapes
 import androidx.compose.material3.Text
+import androidx.compose.material3.VerticalDivider
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.dp
 import com.github.osuasdt.groundstation.ui.theme.GroundstationTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,24 +44,32 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             PageContainer { modifier ->
-                Greeting("blah", modifier)
+                DeploymentInfo(modifier)
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun SingleChannelInfo(modifier: Modifier) {
+    Column(horizontalAlignment = Alignment.CenterHorizontally) {
+        Text("1")
+        Box(Modifier.clip(CircleShape).background(MaterialTheme.colorScheme.primaryContainer)) {
+            Text("name", modifier = Modifier.padding(PaddingValues(8.dp, 0.dp)), color = MaterialTheme.colorScheme.onPrimaryContainer)
+        }
+        Row {
+            Box(modifier = Modifier.size(10.dp).clip(CircleShape).background(Color.Green).align(Alignment.CenterVertically))
+            Spacer(Modifier.width(5.dp))
+            Text("status")
+        }
+    }
 }
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    GroundstationTheme {
-        Greeting("Android")
+fun DeploymentInfo(modifier: Modifier) {
+    Row(modifier.fillMaxWidth().border(1.dp, MaterialTheme.colorScheme.secondary, RoundedCornerShape(10.dp)), horizontalArrangement = Arrangement.SpaceEvenly) {
+        SingleChannelInfo(modifier)
+        SingleChannelInfo(modifier)
+        SingleChannelInfo(modifier)
     }
 }
